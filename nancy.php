75b0177492434aedf47ab1b2b6405c36400c70f3
Joshua Jones <joshua@general-metrics.com>
2009-11-07 15:52:01 -0800

    Cleaned up the formatting.

--- nancy.php
+++ nancy.php
@@ -6,19 +6,23 @@
  *
  *	2009 (c) Copyright The General Metrics Web Development Company
  *	License is do whatever you want.
+ *
+ *	== TODO =====================================================
+ *	=	* Google Sitemap builder
+ *	=	* Navigation builder
+ *	=============================================================
 */
 
-
-define("Version",			"0.1");
-define("Author",			"Joshua Jones");
-define("VIEWS_DIR",			"views");
-define("DEFAULT_TEMPLATE",	"layout.php");
+define('Version',			'0.1');
+define('Author',			'Joshua Jones');
+define('VIEWS_DIR',			'views');
+define('DEFAULT_TEMPLATE',	'layout.php');
 
 
 /*
  *	URI DISPATCH
  *	Interpert the incoming URI
-*/
+ */
 function uri_dispatch()
 {
 	$uri = $_SERVER['PATH_INFO'];
@@ -62,7 +66,6 @@ function layout($template)
 	}
 		else
 	{
-	
 		if (file_exists(APP_PATH . VIEWS_DIR . "/" . $num_uri . ".php"))
 		{
 			$include = APP_PATH . VIEWS_DIR . "/" . $which . ".php";
@@ -72,13 +75,18 @@ function layout($template)
 			$include = APP_PATH . VIEWS_DIR . "/404.php";
 		}
 	}
-	
 	$layout = include(APP_PATH . VIEWS_DIR . "/" . DEFAULT_TEMPLATE);
 }
 
 /*
  *	HELPERS
  *
+ */
+ 
+/*
+ *	STYLESHEETS
+ *	-----------------------
+ *	Load in 1 or more CSS files. Caching control is added in.
  */
 function stylesheets($styles)
 {
@@ -89,7 +97,6 @@ function stylesheets($styles)
 		foreach ($styles as $style)
 		{
 			$mod = filemtime(APP_PATH . "public/stylesheets/" . $style[0] . ".css");
-			
 			$css .= '<link rel="stylesheet" href="/stylesheets/' . $style[0] . '.css';
 			$css .= '?' . $mod . '" ';
 			$css .= 'media="'. $style[1] . '" ';
@@ -97,38 +104,54 @@ function stylesheets($styles)
 			$css .= "\r\t\t";
 		}
 	}
-	
-	else
-	{
-		$css .= "hi";
-	}
-	
 	echo $css;
 }
 
 /*
- *	Google Analytics
+ *	JAVASCRIPTS
+ *	-----------------------
+ *	Load in 1 or more JavaScript files. Caching control is added in.
  */
+function javascripts($js)
+{
+	if (is_array($js))
+	{
+		$scriptfile = "";
+		
+		foreach ($js as $scripts)
+		{
+			$mod = filemtime(APP_PATH . "public/javascripts/" . $scripts[0] . ".css");
+			$scriptfile .= '<script type="text/javascript" src="/javascripts/' . $scripts[0] . '.js';
+			$scriptfile .= '?' . $mod . '"';
+			$scriptfile .= '"></script>';
+			$scriptfile .= "\r\t\t";
+		}
+	}
+	echo $scriptfile;
+}
+
+/*
+ *	Google Analytics
+ *	-------------------------
+ *	A cleaner way to insert Google Analytics code (legacy) in your website.
+ */
+ 
 function google_analytics($gid)
 {
-	$ga  = '<script type="text/javascript">';
-	$ga .= 'var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");';
-	$ga .= 'document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));';
-	$ga .= '</script><script type="text/javascript">';
-	$ga .= 'try {var pageTracker = _gat._getTracker("' . $gid . '");pageTracker._trackPageview();} catch(err) {}';
-	$ga .= '</script>';
-	$ga .= "\r";
-	echo $ga;
+	if ($gid)
+	{
+		$ga  = '<script type="text/javascript">';
+		$ga .= 'var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");';
+		$ga .= 'document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));';
+		$ga .= '</script><script type="text/javascript">';
+		$ga .= 'try {var pageTracker = _gat._getTracker("' . $gid . '");pageTracker._trackPageview();} catch(err) {}';
+		$ga .= '</script>';
+		$ga .= "\r";
+		echo $ga;
+	}
 }
 
-/*
- *	RUN!!!!!!
- */
-function run()
-{
-	layout();
-}
+/*	RUN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+function run(){layout();}
 
-/*
- *	/end of nancy.php
- */
+/* End of nancy.php */
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 define("SITE", "Meow");
 define("APP_PATH", dirname(dirname(__FILE__)) . "/");
 

d450c2ad2ece54345534b9a9063602d53e179053
Joshua Jones <joshua@general-metrics.com>
2009-11-07 16:03:50 -0800

    Moved core files into an 'app' folder.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 
 define("SITE", "Meow");
-define("APP_PATH", dirname(dirname(__FILE__)) . "/");
+define("APP_PATH", dirname(dirname(__FILE__)) . "/app/");
 
 require_once APP_PATH . 'nancy.php';
 

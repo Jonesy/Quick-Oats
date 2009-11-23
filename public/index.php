1c7b15ad96327b0ddff15a957467a2503fcdd09b
Joshua Jones <joshua@general-metrics.com>
2009-11-23 12:55:31 -0800

    Fixed bug with a non-updating index file after merge.

--- public/index.php
+++ public/index.php
@@ -1,15 +1,20 @@
 <?php
 
-error_reporting(E_ALL);
+# Error Reporting
+ini_set('display_errors', TRUE);
+error_reporting(E_ALL & ~E_STRICT);
 
-define('Version',			'0.1');
-define('SITE_NAME',			'Demo Site');
-define("PUBLIC_PATH",		dirname(dirname(__FILE__)) . "/");
-define("APP_PATH",			dirname(dirname(__FILE__)) . "/app/");
-require_once APP_PATH . '/config.php';
-require_once APP_PATH . 'quaker.php';
+# Define some stuff
+define('Version',	'0.1');
+define("APP_PATH",	dirname(dirname(__FILE__)) . "/app");
 
-$oats = new Quaker();
+include APP_PATH . '/config.php';
+
+# Put the pot on
+require_once APP_PATH . '/quaker.php';
 
 
-/* End of index.php */
+#Bring to boil
+Quaker::boil($config);
+
+# End of index.php
